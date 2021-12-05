b42c0630314bab93461e13232457716bbaea597c
Tim Searle <tim@searle.dev>
2021-12-05 10:06:11 +0000

    Day 5 part 1

--- AdventOfCode2021/Day Five/DayFive.swift
+++ AdventOfCode2021/Day Five/DayFive.swift
@@ -1,13 +1,58 @@
 import Foundation
 
-public final class DayFive {
+public struct Vent {
+    public let startPos: Coordinate
+    public let endPos: Coordinate
     
-    public init() {
-        
+    public init(startPos: Coordinate, endPos: Coordinate) {
+        self.startPos = startPos
+        self.endPos = endPos
+    }
+}
+
+public struct Coordinate: Hashable {
+    public let xPos: Int
+    public let yPos: Int
+    
+    public init(xPos: Int, yPos: Int) {
+        self.xPos = xPos
+        self.yPos = yPos
+    }
+}
+
+public final class DayFive {
+    let vents: [Vent]
+    
+    public init(vents: [Vent]) {
+        self.vents = vents
     }
     
     public func partOne() -> Int {
-        0
+        var seabed = [Coordinate: Int]()
+        
+        vents.forEach {
+            markLocations($0, on: &seabed)
+        }
+        
+        return seabed.values.filter { $0 != 1 }.count
+    }
+    
+    private func markLocations(_ vent: Vent, on seabed: inout [Coordinate: Int]) {
+        // Only caring about horizontal or vertical lines
+        
+        if vent.startPos.xPos == vent.endPos.xPos {
+            // increment/decrement through y
+            let range = vent.startPos.yPos > vent.endPos.yPos ? vent.endPos.yPos...vent.startPos.yPos : vent.startPos.yPos...vent.endPos.yPos
+            for i in range {
+                seabed[Coordinate(xPos: vent.startPos.xPos, yPos: i), default: 0] += 1
+            }
+        } else if vent.startPos.yPos == vent.endPos.yPos {
+            // increment/decrement through x
+            let range = vent.startPos.xPos > vent.endPos.xPos ? vent.endPos.xPos...vent.startPos.xPos : vent.startPos.xPos...vent.endPos.xPos
+            for i in range {
+                seabed[Coordinate(xPos: i, yPos: vent.startPos.yPos), default: 0] += 1
+            }
+        }
     }
     
     public func partTwo() -> Int {
--- AdventOfCode2021Tests/DayFiveTests.swift
+++ AdventOfCode2021Tests/DayFiveTests.swift
@@ -2,31 +2,41 @@ import XCTest
 import AdventOfCode2021
 
 class DayFiveTests: XCTestCase {
-    private func makeInput(sample: Bool = false) -> Void {
+    private func makeInput(sample: Bool = false) -> [Vent] {
         let inputURL = Bundle(for: type(of: self)).url(forResource: sample ? "DayFive-sample" : "DayFive", withExtension: "txt")!
-    }
-
-    func testDayFive_1_sample() {
-        _ = makeInput(sample: true)
-        let dayFive = DayFive()
-        XCTAssertEqual(dayFive.partOne(), -1)
-    }
-
-    func testDayFive_1() {
-        _ = makeInput()
-        let dayFive = DayFive()
-        XCTAssertEqual(dayFive.partOne(), -1)
-    }
-
-    func testDayFive_2_sample() {
-        _ = makeInput(sample: true)
-        let dayFive = DayFive()
-        XCTAssertEqual(dayFive.partTwo(), -1)
+        return try! String(contentsOf: inputURL)
+            .components(separatedBy: .newlines)
+            .dropLast()
+            .map { $0.components(separatedBy: " -> ") }
+            .flatMap {
+                let startNums = $0[0].components(separatedBy: ",").map { Int($0)! }
+                let endNums = $0[1].components(separatedBy: ",").map { Int($0)! }
+                return Vent(startPos: Coordinate(xPos: startNums[0], yPos: startNums[1]),
+                            endPos: Coordinate(xPos: endNums[0], yPos: endNums[1]))
+            }
     }
     
-    func testDayFive_2() {
-        _ = makeInput()
-        let dayFive = DayFive()
-        XCTAssertEqual(dayFive.partTwo(), -1)
+    func testDayFive_1_sample() {
+        let vents = makeInput(sample: true)
+        let dayFive = DayFive(vents: vents)
+        XCTAssertEqual(dayFive.partOne(), 5)
     }
+    
+    func testDayFive_1() {
+        let vents = makeInput()
+        let dayFive = DayFive(vents: vents)
+        XCTAssertEqual(dayFive.partOne(), 6461)
+    }
+//
+//    func testDayFive_2_sample() {
+//        _ = makeInput(sample: true)
+//        let dayFive = DayFive()
+//        XCTAssertEqual(dayFive.partTwo(), -1)
+//    }
+//
+//    func testDayFive_2() {
+//        _ = makeInput()
+//        let dayFive = DayFive()
+//        XCTAssertEqual(dayFive.partTwo(), -1)
+//    }
 }
